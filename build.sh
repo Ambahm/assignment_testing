f2e225f5c0e557320d8ade20e6557c96c9feebf5
Chad Heuschober <chad.heuschober@cuny.edu>
2014-08-26 00:33:47 -0400

    A better way to run pylint.

--- build.sh
+++ build.sh
@@ -8,7 +8,10 @@ LOGDIR="${TESTROOT}/target"
 export PYTHONPATH="${PYTHONPATH}:${TESTROOT}:${SRCROOT}"
 
 # lint tests
-pylint --rcfile="${TESTROOT}/pylintrc" "${SRCROOT}" | tee "${LOGDIR}/pylint.log"
+find "${SRCROOT}" -type f -iname "*.py" -print0 | while read -d $'\0' file
+do
+    pylint --rcfile="${TESTROOT}/pylintrc" "${file}" | tee -a "${LOGDIR}/pylint.log"
+done
 
 # unit tests
 nosetests -w "${TESTROOT}/unit/${LESSON_ID}" --with-xunit --xunit-file="${LOGDIR}/nosetest.xml"
