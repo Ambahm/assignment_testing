736cbe079c37943790a6187ce8bf2365371fb028
Chad Heuschober <chad.heuschober@cuny.edu>
2014-08-26 00:46:41 -0400

    Trying to make paths relative for lint tests for compatibility with violations plugin.

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-SRCROOT="${WORKSPACE}/src"
-TESTROOT="${WORKSPACE}/testing"
+SRCROOT="src"
+TESTROOT="testing"
 LOGDIR="${TESTROOT}/target"
 
 # might need to change this to sr
-export PYTHONPATH="${PYTHONPATH}:${TESTROOT}:${SRCROOT}"
+export PYTHONPATH="${PYTHONPATH}:${WORKSPACE}/${TESTROOT}:${WORKSPACE}/${SRCROOT}"
+
+# get started in the right location
+cd "${WORKSPACE}"
 
 # lint tests
 find "${SRCROOT}" -type f -iname "*.py" -print0 | while read -d $'\0' file
